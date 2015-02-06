01883094d49b4f7ad25ff6738f79def13df74fe9
kurdishsamurai <alan.dabagh@gmail.com>
2015-02-06 15:10:51 +0100

    updates

--- src/TestReplace.java
+++ src/TestReplace.java
@@ -5,9 +5,10 @@ import org.junit.Test;
 
 public class TestReplace {
 
+	Server tester = new Server();
 	@Test
-	public void test() {
-		fail();
+	public void testNotExistingID() {
+		assertEquals("non existing id should return -1", -1, tester.replace(6523223, "howdy"));
 	}
 
 }
