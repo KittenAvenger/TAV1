771f2cb629613f3954a77c8a3286a2cb55790231
kurdishsamurai <alan.dabagh@gmail.com>
2015-02-06 15:50:44 +0100

    test case done

--- src/TestReplace.java
+++ src/TestReplace.java
@@ -6,9 +6,29 @@ import org.junit.Test;
 public class TestReplace {
 
 	Server tester = new Server();
+	
 	@Test
-	public void testNotExistingID() {
+	public void NotExistingID() {
 		assertEquals("non existing id should return -1", -1, tester.replace(6523223, "howdy"));
+	}
+	
+	@Test
+	public void cantReplaceFetched(){
+		int ID = tester.add("howdy mate", "0768897676", "0778637453");
+		tester.fetch("0778637453");
+		assertEquals("this message is already fetched, should return -1", -1, tester.replace(ID, "this text should not be replaced"));
+	}
+	
+	@Test
+	public void checkEmptyMessage(){
+		int ID = tester.add("hello matey", "0763243561", "0776784563");
+		assertEquals("this message is empty, should return -1", -1, tester.replace(ID, ""));
+	}
+	
+	@Test
+	public void normalFunction(){
+		int ID = tester.add("whats up", "0763654561", "0776782163");
+		assertEquals("Should return the message ID", ID, tester.replace(ID, "changed message"));
 	}
 
 }
