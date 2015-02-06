25f687eb03fa852be5d8d326492c6147c5f2da0f
kurdishsamurai <alan.dabagh@gmail.com>
2015-02-06 16:30:45 +0100

    test case done

--- src/TestDelete.java
+++ src/TestDelete.java
@@ -22,21 +22,20 @@ public class TestDelete {
 	
 	@Test
 	public void messageFetched() {
-		tester.add("Hey what is up", "0777777777", "0766666666");
-		int IDs = tester.returnServer().getID();
+		int IDs = tester.add("Hey what is up", "0777777777", "0766666666");
 		tester.returnServer().setFetched();
 		assertEquals("if message fetched return error", -1, tester.delete(IDs));
 	}
 	
 	@Test
 	public void normalFunction() {
-		tester.add("Hey what is up", "0777777777", "0766666666");
-		int IDs = tester.returnServer().getID();
-		assertEquals("Hey what is up", tester.returnServer().getMessage());
+		int IDs = tester.add("Hey what is up", "0777777777", "0766666666");
+		System.out.println(tester.returnServer().getMessage());
+		
+		assertEquals("test", "Hey what is up", tester.returnServer().getMessage());
 		assertEquals("if message not fetched and ID is correct return ID", IDs, tester.delete(IDs));
 		
-		
-	}
+		}
 	
 	
 	
